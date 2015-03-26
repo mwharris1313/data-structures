d1b428e6807cc47b4e44d459e03d84a554728a1c
mwharris333 <mwharris333@gmail.com>
2015-03-25 20:07:32 -0700

    completed function-shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,35 @@
 var Queue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var q = {};
+  q.sizeStorage = 0;
+  q.storage = {};
+  q.enqueue = queueMethods.enqueue;
+  q.dequeue = queueMethods.dequeue;
+  q.size = queueMethods.size;
+
+  return q;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.sizeStorage] = value;
+  this.sizeStorage++;
+}
+
+queueMethods.dequeue = function() {
+  var temp = this.storage[0];
+  if(this.sizeStorage > 0) {
+    var keys = Object.keys(this.storage);
+    for(var i = 1; i < keys.length; i++) {
+      this.storage[i - 1] = this.storage[i];
+    }
+    this.sizeStorage--;
+  }
+  return temp;
+}
+
+queueMethods.size = function(){
+  return this.sizeStorage;
+}
 
 
