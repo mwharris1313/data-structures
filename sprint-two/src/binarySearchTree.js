1d262df1de76e17ac94cc5e6ed7e019ceba27940
mwharris333 <mwharris333@gmail.com>
2015-03-27 16:40:17 -0700

    finished half of binarysearchtree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,31 @@
 var BinarySearchTree = function(value){
-};
+  var obj = {};
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+  obj.insert = function(value){
+    if(value < obj.value) {
+      if(obj.left === null) {
+        obj.left = BinarySearchTree(value);
+      } else {
+        obj.left.insert(value);
+      }
+    } else {
+      if(obj.right === null) {
+        obj.right = BinarySearchTree(value);
+      } else {
+        obj.right.insert(value);
+      }
+    }
+  };
+  obj.contains = function(){
 
+  };
+  obj.depthFirstLog = function(){
+
+  };
+  return obj;
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
